bad5b66c72ddb611ddbab971e771b429a7ca5559
Ryan Armstrong <ryan@cavaliercoder.com>
2016-02-11 22:14:15 +0800

    Moved error tests into discrete test file

--- badio_test.go
+++ badio_test.go
@@ -5,16 +5,6 @@ import (
 	"testing"
 )
 
-func TestIsBadIOError(t *testing.T) {
-	if IsBadIOError(fmt.Errorf("Not a BadIOError")) {
-		t.Errorf("IsBadIOError returned true for a non-BadIOError error")
-	}
-
-	if !IsBadIOError(newError("A BadIOError")) {
-		t.Errorf("IsBadIOError returned false for a BadIOError error")
-	}
-}
-
 func TestBreakReader(t *testing.T) {
 	// reader to generate infinite data stream
 	tr := NewNullReader()
---
+++ errors_test.go
@@ -0,0 +1,15 @@
+package badio
+
+import (
+	"testing"
+)
+
+func TestIsBadIOError(t *testing.T) {
+	if IsBadIOError(fmt.Errorf("Not a BadIOError")) {
+		t.Errorf("IsBadIOError returned true for a non-BadIOError error")
+	}
+
+	if !IsBadIOError(newError("A BadIOError")) {
+		t.Errorf("IsBadIOError returned false for a BadIOError error")
+	}
+}
